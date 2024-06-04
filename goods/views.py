5ee4d6dc715faeba9d1cf599334b06e3a4879f49
Efrem <efrempavlov@yandex.ru>
2024-06-04 16:42:28 +0300

    pagination with GET params

--- goods/views.py
+++ goods/views.py
@@ -4,13 +4,14 @@ from django.core.paginator import Paginator
 from goods.models import Categories, Products
 
 
-def catalog(request, category_slug, page=1):
+def catalog(request, category_slug):
     if category_slug == "all":
         goods = Products.objects.all()
     else:
         goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
 
     paginator = Paginator(goods, 3)
+    page = int(request.GET.get("page", 1))
     current_page = paginator.page(page)
 
     context = {
