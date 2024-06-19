410d758800d504b27bfa2d72cb0898080cff9aff
Efrem Pavlov <efrempavlov@yandex.ru>
2024-06-19 11:58:03 +0300

    first cart backend structure

--- app/settings.py
+++ app/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     "main",
     "goods",
     "users",
+    "carts",
 ]
 
 MIDDLEWARE = [
--- app/urls.py
+++ app/urls.py
@@ -21,8 +21,8 @@ from django.conf.urls.static import static
 
 from app import settings
 import goods.urls
-from main import views
 import users.urls
+import carts.urls
 
 
 
@@ -32,7 +32,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls', namespace="main")),
     path("catalog/", include(goods.urls, namespace="catalog")),
-    path("user/", include(users.urls, namespace="user"))
+    path("user/", include(users.urls, namespace="user")),
+    path("cart/", include(carts.urls, namespace="cart"))
 ]
 
 if settings.DEBUG:
