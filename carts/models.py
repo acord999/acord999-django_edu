4f9bda9f567a834e86603c3d6c06787762ab7ad5
Efrem Pavlov <efrempavlov@yandex.ru>
2024-06-19 16:27:20 +0300

    cart && templates

--- carts/models.py
+++ carts/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+from goods.models import Products
+
+
+class CartQueryset(models.QuerySet):
+
+    def total_price(self):
+        return sum(cart.product_price() for cart in self)
+
+    def total_quantity(self):
+        if self:
+            return sum(cart.quantity for cart in self)
+        return 0
+
+
+class Cart(models.Model):
+
+    user = models.ForeignKey(
+        to=User,
+        on_delete=models.CASCADE,
+        verbose_name="Пользователь",
+        blank=True,
+        null=True,
+    )
+    product = models.ForeignKey(
+        to=Products, 
+        on_delete=models.CASCADE, 
+        verbose_name="Товар"
+    )
+    session_key = models.CharField(max_length=32, blank=True, null=True)
+    quantity = models.SmallIntegerField(default=0, verbose_name="Количество")
+    created_timestamp = models.DateTimeField(
+        auto_now_add=True, verbose_name="Дата добавления"
+    )
+
+    objects = CartQueryset().as_manager()
+
+    class Meta:
+        db_table = "cart"
+        verbose_name = "Корзина"
+        verbose_name_plural = "Корзина"
+
+    def product_price(self):
+        return round(self.product.sell_price() * self.quantity)
+
+    def __str__(self):
+        return f"Корзина пользователя {self.user.username} | Товар {self.product.name} | Количество {self.quantity}"
--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.contrib import messages
 from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 from django.urls import reverse
@@ -10,8 +10,8 @@ from users.forms import ProfileForm, UserLoginForm, UserRegistrationForm
 
 
 def login(request):
-    # Проверка была ли попытка авторизации 
-    if request.method == 'POST':
+    # Проверка была ли попытка авторизации
+    if request.method == "POST":
         form = UserLoginForm(data=request.POST)
         # Проверка на соответстие форме !ТОЧНЫЕ НАЗВАНИЯ ПОЛЕЙ х)!
         if form.is_valid():
@@ -23,18 +23,20 @@ def login(request):
             if user:
                 # Авторизация на сайте
                 auth_login(request, user)
-                messages.success(request, f"{user.username}, Вы успешно вошли в аккаунт")
+                messages.success(
+                    request, f"{user.username}, Вы успешно вошли в аккаунт"
+                )
                 # Переброс на целевую страницу после успешной авторизации
-                
-                if request.POST.get('next', None):
-                    return HttpResponseRedirect(request.POST.get('next'))
-                
-                return HttpResponseRedirect(reverse('main:index'))
+
+                redirect_page = request.POST.get("next", None)
+                if redirect_page and redirect_page != reverse("user:logout"):
+                    return HttpResponseRedirect(request.POST.get("next"))
+
+                return HttpResponseRedirect(reverse("main:index"))
     # Иначе считаем, что пользователь не пытался авторизироваться и подготавливаем страничку авторизации
     else:
         form = UserLoginForm()
 
-    
     context = {
         "title": "Авторизация - Home",
         "form": form,
@@ -43,8 +45,8 @@ def login(request):
 
 
 def registration(request):
-     # Проверка была ли попытка авторизации 
-    if request.method == 'POST':
+    # Проверка была ли попытка авторизации
+    if request.method == "POST":
         form = UserRegistrationForm(data=request.POST)
         # Проверка на соответстие форме !ТОЧНЫЕ НАЗВАНИЯ ПОЛЕЙ х)!
         if form.is_valid():
@@ -52,30 +54,31 @@ def registration(request):
             user = form.instance
             messages.success(request, f"{user.username}, Вы успешно создали аккаунт")
             auth_login(request, user)
-            return HttpResponseRedirect(reverse('main:index'))
+            return HttpResponseRedirect(reverse("main:index"))
     # Иначе считаем, что пользователь не пытался авторизироваться и подготавливаем страничку авторизации
     else:
         form = UserRegistrationForm()
 
-    
     context = {
         "title": "Регистрация - Home",
         "form": form,
     }
     return render(request, "users/registration.html", context=context)
 
+
 @login_required
 def profile(request) -> HttpResponseRedirect | HttpResponse:
-    if request.method == 'POST':
-        form = ProfileForm(data=request.POST, instance=request.user, files=request.FILES)
+    if request.method == "POST":
+        form = ProfileForm(
+            data=request.POST, instance=request.user, files=request.FILES
+        )
         if form.is_valid():
             form.save()
             messages.success(request, "Профайл успешно обновлен")
-            return HttpResponseRedirect(reverse('user:profile'))
+            return HttpResponseRedirect(reverse("user:profile"))
     # Иначе считаем, что пользователь не пытался авторизироваться и подготавливаем страничку авторизации
     else:
         form = ProfileForm(instance=request.user)
-
 
     context = {
         "title": "Кабинет - Home",
@@ -83,8 +86,13 @@ def profile(request) -> HttpResponseRedirect | HttpResponse:
     }
     return render(request, "users/profile.html", context=context)
 
+
 @login_required
 def logout(request):
     messages.success(request, f"{request.user.username}, Вы вышли из аккаунта")
     auth_logout(request)
-    return HttpResponseRedirect(reverse('main:index'))
+    return HttpResponseRedirect(reverse("main:index"))
+
+
+def users_cart(request):
+    return render(request, "users/users_cart.html")
