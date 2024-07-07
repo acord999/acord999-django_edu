f011955c6a9a9023b14a230fc42b0469296f1c33
Efrem <contact@efrempavlov.ru>
2024-07-07 14:35:26 +0300

    Change cart ajax

--- carts/views.py
+++ carts/views.py
@@ -1,6 +1,5 @@
-from urllib import response
+
 from django.http import JsonResponse
-from django.shortcuts import redirect, render
 from django.template.loader import render_to_string
 from carts.models import Cart
 from carts.utils import get_user_carts
@@ -38,8 +37,27 @@ def cart_add(request):
     return JsonResponse(response_data)
             
 
-def cart_change(request, product_slug):
-    ...
+def cart_change(request):
+        cart_id = request.POST.get("cart_id") 
+        quantity = request.POST.get("quantity")
+        
+        cart = Cart.objects.get(id=cart_id)
+
+        cart.quantity = quantity
+        cart.save()
+
+        user_cart = get_user_carts(request=request)
+        cart_items_html = render_to_string(
+            "carts/includes/included_cart.html", {"carts": user_cart}, request=request)
+        response_data = {
+            "message": "Товар удален из корзины",
+            "cart_items_html": cart_items_html,
+            "quantity": quantity,
+        }
+        return JsonResponse(response_data)
+    
+        
+
 
 def cart_remove(request):
     
